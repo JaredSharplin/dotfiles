cc003ebd4c017fcb13854be642185ea23e2e80cf
Jared Sharplin <jaredsharplin1@gmail.com>
2025-11-06 17:16:54 +1000

    95% width on pickers

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        layout = {
+          layout = {
+            width = 0.95, -- 95% of screen width
+            height = 0.95, -- 95% of screen height
+          },
+        },
+      },
+    },
+  },
+}
