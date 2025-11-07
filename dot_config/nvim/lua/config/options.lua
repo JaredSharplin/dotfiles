242b8c4af0bed91f6fd352897ff7c111a9ad6750
Jared Sharplin <jared@tanda.co>
2025-11-07 20:42:24 +1000

    Get rid of weird blue stuff in diff

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Fix blue background in diff mode
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "DiffChange", { bg = "NONE" })
+  end,
+})
